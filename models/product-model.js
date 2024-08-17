a26d35018d073c6202b6c3ae9c24b676cef9e6d7
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 03:56:21 -0700

    products felds

--- models/product-model.js
+++ models/product-model.js
@@ -2,7 +2,23 @@ import mongoose from "mongoose";
 
 const prodctSchema= mongoose.Schema({
 
-
+name:{
+    typeof:String,
+    required:true,
+    
+},
+image:String,
+price:{
+    typeof:Number,
+    required:true,
+    },
+ discount:{ 
+    typeof:Number,
+    default:0,
+    },
+bgcolor:String,
+panelcolor:String,
+textcolor:String,
 }
 ,
      { timestamps: true})
