a3fbd61d2cb36fa774d32bd666a66bd02c02fa1b
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 03:49:01 -0700

    user fields

--- models/user-model.js
+++ models/user-model.js
@@ -1,10 +1,42 @@
 import mongoose from "mongoose";
 
 const userSchema= mongoose.Schema({
+username:{
+    typeof:String,
+    required:true,
+    trim:true,
+},
+email:{
+    typeof:String,
+    required:true,
+    },
+ password:{
+    typeof:String,
+    required:true,
+    },
+cart:{
+    typeof:Array,
+    default:[],
+    },
+isAdmin:{
+    typeof:Boolean,
+    
+    },
+orders:{
+    typeof:Array,
+    default:[],
+    },
+contact:{
+    typeof:Number,
+    
+    },
+image:{
+    typeof:String,
+    
+    },
 
 
-}
-,
-     { timestamps: true})
+
+},{ timestamps: true})
 module.exports= mongoose.model("User",userSchema)
      
