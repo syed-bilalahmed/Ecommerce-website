c92e0b9259b3c1b3451563f822c3d057ceb6eea9
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 10:59:39 -0700

    add User register routee

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,8 +1,18 @@
 const  express = require("express");
 const router = express.Router(); 
-
+const userModel = require("../models/user-model")
 router.get("/",(req,res)=>{
-    res.send("Uers Router")
+    res.send("Ueser Router")
 })
+
+router.post("/register",  async (req,res)=>{
+    // res.send("Uers Router")
+    let {username, emmail, password }=req.body;
+    let newUser= await userModel.create({
+        username,
+        email,
+        password,
+    })
+});
 
 module.exports= router;
