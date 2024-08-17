7de4d01f7e6c51e58cae2ba8ef902ebee9e88ea0
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 04:38:47 -0700

    routes

--- index.js
+++ index.js
@@ -2,12 +2,23 @@ const express = require('express');
 const app = express();
 const cookieParser = require('cookie-parser');
 const path = require('path');
+const db= require("./config/mongoose-connection")
+
+const ownersRouter=require("./routes/onwersRouter");
+const usersRouter=require("./routes/usersRouter");
+const productsRouter=require("./routes/productsRouter");
 
 // Use middleware functions correctly
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
+app.use("/owner",ownersRouter);
+app.use("/users",usersRouter);
+app.use("/products",productsRouter);
 
 // Set the view engine
 app.set('view engine', 'ejs');
