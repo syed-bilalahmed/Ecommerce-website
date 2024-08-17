6236ef31fa33892129d89966a11ce28471693d4d
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 12:11:01 -0700

    hashing password and JWT tokken creation

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,18 +1,43 @@
-const  express = require("express");
-const router = express.Router(); 
-const userModel = require("../models/user-model")
-router.get("/",(req,res)=>{
-    res.send("Ueser Router")
-})
+const express = require("express");
+const router = express.Router();
+const userModel = require("../models/user-model");
+const bcrypt = require("bcrypt");
+const jwt=require("jsonwebtoken");
 
-router.post("/register",  async (req,res)=>{
-    // res.send("Uers Router")
-    let {username, emmail, password }=req.body;
-    let newUser= await userModel.create({
-        username,
-        email,
-        password,
-    })
+
+
+
+router.get("/", (req, res) => {
+  res.send("Ueser Router");
 });
 
-module.exports= router;
+router.post("/register", async (req, res) => {
+  // res.send("User Register Route")
+  //    console.log("user regsitertation Route");
+
+  try {
+    let { username, email, password } = req.body;
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(password, salt, async (err, hash) => {
+        if (err) return res.send(err.message);
+        else {
+          let newUser = await userModel.create({
+            username,
+            email,
+            password: hash,
+          });
+         
+          let token=jwt.sign({email, id:newUser._id},  process.env.JWT_KEY, { expiresIn: '1h' }) 
+res.cookie("token",token)
+res.send("user created sucsffully");
+
+          // res.send(newUser);
+        }
+      });
+    });
+  } catch (error) {
+    res.send(error.message);
+  }
+});
+
+module.exports = router;
