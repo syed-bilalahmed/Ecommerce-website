da582480b4ec73693beadffad7990543c37e7ba3
Syed BilalAhmed <syedbilalahmed5292@gmail.com>
2024-08-17 03:52:44 -0700

    onwer fields

--- models/owner-model.js
+++ models/owner-model.js
@@ -1,8 +1,23 @@
 import mongoose from "mongoose";
 
 const ownerSchema= mongoose.Schema({
-
-
+username:{
+    typeof:String,
+    required:true,
+    trim:true,
+},
+email:{
+    typeof:String,
+    required:true,
+    },
+ password:{
+    typeof:String,
+    required:true,
+    },
+    products:{
+        typeof:String
+    },
+     
 }
 ,
      { timestamps: true})
